5fddb4a92d1bf4090d5b81e6216b8ee8a01cccdf
mahantheshhv <mahantheshhv@gmail.com>
2015-08-20 11:33:05 +0530

    snippet to read from amazon s3

--- spark_udf_snippets/read_S3_file.py
+++ spark_udf_snippets/read_S3_file.py
@@ -1,9 +1,11 @@
 
 # Code snippet to read file from s3 on PySpark 
 
+#create the spark context sc and use hadoopFile api or newAPIHadoopFile
+
 rdd = sc.hadoopFile('s3n://<path to the file>',
 
-				'org.apache.hadoop.mapred.TextInputFormat',
+		'org.apache.hadoop.mapred.TextInputFormat',
                     'org.apache.hadoop.io.Text',
                     'org.apache.hadoop.io.LongWritable',
                      conf = {
